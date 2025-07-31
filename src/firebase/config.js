40a439ef92f9b58a2094a356757d9d06789dcbc6
RohanJP27 <srohanjp@gmail.com>
2025-07-30 20:53:01 -0700

    Fix: Include Firebase config file in deployment and remove from .gitignore

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZbVYuOa4_0d0tzPjNRZnRTB179Yb82JQ",
+  authDomain: "linkedn-clone-5861f.firebaseapp.com",
+  projectId: "linkedn-clone-5861f",
+  storageBucket: "linkedn-clone-5861f.firebasestorage.app",
+  messagingSenderId: "524347573693",
+  appId: "1:524347573693:web:7fff9885294679c1ad3ed4",
+  measurementId: "G-GWD1WQWEL9"
+};
+
+export default firebaseConfig; 
